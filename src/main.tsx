3e05fbe0f805ae8c1ca4a8c6f3407d2b107212e9
Holy-Grm <oligermain15@gmail.com>
2025-09-12 16:32:04 -0400

    debug 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { AutoVisuPage } from "./components/AutoVisuPage";
 import { NotFoundPage } from "./components/NotFoundPage";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/autovisu" element={<AutoVisuPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
   
