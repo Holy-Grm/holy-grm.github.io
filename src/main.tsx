a18db6db42d1d7910c78fdffa36badbad73dd1ef
Holy-Grm <oligermain15@gmail.com>
2025-12-08 23:18:13 -0500

    correction d'orthographe

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { AutoVisuPage } from "./components/AutoVisuPage";
 import { PdfMergePage } from "./components/PdfMergePage";
-import { PdfTrimmerPage } from "./components/PdfTrimmerPage";
+import { PdfTrimPage } from "./components/PdfTrimPage";
 import { NotFoundPage } from "./components/NotFoundPage";
 import "./index.css";
 
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <Route path="/" element={<App />} />
       <Route path="/autovisu" element={<AutoVisuPage />} />
       <Route path="/pdfmerge" element={<PdfMergePage />} />
-      <Route path="/pdfTrim" element={<PdfTrimmerPage />} />
+      <Route path="/pdfTrim" element={<PdfTrimPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </HashRouter>
